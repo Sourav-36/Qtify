c84bfb74ea3d7db3926e113eaea729d072a0e762
Sourav-36 <souravnayak.nayak@gmail.com>
2023-11-25 12:06:32 +0530

    change route path

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/home" component={HomePage} />
+        <Route exact path="/" component={HomePage} />
       </Switch>
     </div>
   );
